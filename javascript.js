2309fd148d880690fe160238c645f05ec4564e99
gigi1417 <torresgiselle1997@gmail.com>
2023-07-07 19:18:55 -0400

    Creating conditionals to determine the winner

--- javascript.js
+++ javascript.js
@@ -13,5 +13,43 @@ function getComputerChoice() {
     }
 }
 
-let move = getComputerChoice();
-console.log(move)
+function getPlayerChoice() {
+    let max = 3
+    let min = 1
+    let move = Math.floor(Math.random() * (max - min + 1)) + min;
+    if (move === 1) {
+        return 'Rock';
+    }
+    else if (move === 2) {
+        return 'Paper';
+    }
+    else {
+        return 'Scissors'
+    }
+}
+
+function determinedWinner(playerChoicePassedInTheFunction, computerChoicePassedInTheFunction) {
+    if (playerChoicePassedInTheFunction === computerChoicePassedInTheFunction) {
+        return 'Tie'
+    }
+    if ((playerChoicePassedInTheFunction ==='Rock' && computerChoicePassedInTheFunction === 'Paper') ||
+    (playerChoicePassedInTheFunction === 'Paper' && computerChoicePassedInTheFunction === 'Scissors') ||
+    (playerChoicePassedInTheFunction === 'Scissors' && computerChoicePassedInTheFunction === 'Rock')) {
+        return 'You lose!'
+    }
+
+    if ((computerChoicePassedInTheFunction ==='Rock' && playerChoicePassedInTheFunction === 'Paper') ||
+    (computerChoicePassedInTheFunction === 'Paper' && playerChoicePassedInTheFunction === 'Scissors') ||
+    (computerChoicePassedInTheFunction === 'Scissors' && playerChoicePassedInTheFunction === 'Rock')) {
+        return 'You win!'
+    }
+}
+
+let computerChoice = getComputerChoice();
+console.log("The computer picked: "+computerChoice)
+
+let playerChoice = getPlayerChoice();
+console.log("The player picked: "+playerChoice)
+
+let gameOutcome = determinedWinner(playerChoice, computerChoice);
+console.log(gameOutcome)
